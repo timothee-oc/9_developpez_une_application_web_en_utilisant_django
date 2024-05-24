859c5723f8b2d53ab7c691043ebaa69a36928e85
timothee-oc <timothee.oc@proton.me>
2024-05-24 08:56:49 +0200

    Format review form like ticket form and add border to reviews tickets in feed

--- litrevu/review/forms.py
+++ litrevu/review/forms.py
@@ -15,3 +15,8 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.Review
         exclude = ("user", "ticket")
+        widgets = {
+            "headline": forms.TextInput(attrs={"class": "d-block w-75 m-auto"}),
+            "rating": forms.NumberInput(attrs={"class": "d-block w-75 m-auto"}),
+            "body": forms.Textarea(attrs={"class": "d-block w-75 m-auto"}),
+        }
